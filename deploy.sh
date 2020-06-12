ebbee0ab590aa48921d89513c53075fa0fde4ddc
Bill Compton <bill.compton@mindinmotionsoftware.com>
2020-06-12 13:41:46 -0600

    added

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,10 @@ echo "Deploying to target ..."
 echo "***********************************************************************"
 echo "***********************************************************************"
 
-echo ... but not really yet
+
+set -x
+
+TargetIP=54.213.9.51
+
+scp -i ~/pem/JenkinsServerKeyPair.pem target/actuator-service-0.0.1-SNAPSHOT.jar ubuntu@${TargetIP}:app
+ssh -i ~/pem/JenkinsServerKeyPair.pem ubuntu@${TargetIP} "sudo systemctl restart my-webapp"
