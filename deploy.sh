243f1a1131ccd00f7ac1be01056fb0c2177ba313
Bill Compton <bill.compton@mindinmotionsoftware.com>
2020-06-12 13:55:28 -0600

    inherit app server ip address from environment

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo "***********************************************************************"
 
 set -x
 
-TargetIP=54.213.9.51
+# AppServerIpAddress=54.213.9.51
 
-scp -i ~/pem/JenkinsServerKeyPair.pem target/actuator-service-0.0.1-SNAPSHOT.jar ubuntu@${TargetIP}:app
-ssh -i ~/pem/JenkinsServerKeyPair.pem ubuntu@${TargetIP} "sudo systemctl restart my-webapp"
+scp -i ~/pem/JenkinsServerKeyPair.pem target/actuator-service-0.0.1-SNAPSHOT.jar ubuntu@${AppServerIpAddress}:app
+ssh -i ~/pem/JenkinsServerKeyPair.pem ubuntu@${AppServerIpAddress} "sudo systemctl restart my-webapp"
